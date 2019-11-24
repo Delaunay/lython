28aa59acf9f6da76ac464b375ed8eb44275b08fb
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-11-23 19:04:19 -0500

    add interpreter test able to define args

--- tests/interpreter_test.h
+++ tests/interpreter_test.h
@@ -8,8 +8,21 @@
 
 using namespace lython;
 
+template<typename... Args>
+void insert_arg(AST::Call* call, Args... v){
+    insert_arg(call, v...);
+}
 
-Value interpret_it(String code, String fun_name){
+template<typename T, typename... Args>
+void insert_arg(AST::Call* call, T a, Args... v){
+    call->arguments().push_back(ST::Expr(new AST::ValueExpr(a, nullptr)));
+    insert_arg(call, v...);
+}
+
+void insert_arg(AST::Call*){}
+
+template<typename... Args>
+Value interpret_it(String code, String fun_name, Args... v){
     StringBuffer reader(code);
 
     // Parse code
@@ -32,6 +45,7 @@ Value interpret_it(String code, String fun_name){
         return Value("Unknown function");
 
     call->function() = fun;
+    insert_arg(call, v...);
 
     // return value
     return vm.eval(ST::Expr(call));
@@ -42,6 +56,13 @@ Value interpret_it(String code, String fun_name){
         REQUIRE(interpret_it(code(), #code) == value);\
     }
 
+#define TEST_INTERPRETER_ARGS(code, value, ...)\
+    SECTION(#code){\
+        REQUIRE(interpret_it(code(), #code, __VA_ARGS__) == value);\
+    }
+
+
 TEST_CASE("Interpreter"){
     TEST_INTERPRETER(simple_function_noargs, Value(1.0))
+    // TEST_INTERPRETER_ARGS(simple_function_return_args, Value(1.0), 1.0)
 }
