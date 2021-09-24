78ed55c5978f720f9774169861bb13d89ae1ee30
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-09-23 21:40:15 -0400

    -

--- src/utilities/metadata.h
+++ src/utilities/metadata.h
@@ -6,27 +6,26 @@
 
 namespace lython {
 
-#define TYPES_METADATA(X)                                                                          \
-    X(String, String)                                                                              \
-    X(char, Char)
+#define TYPES_METADATA(X) \
+    X(String, String)     \
+    X(char, Char)         \
+    X(int8, Int8)         \
+    X(int16, Int16)       \
+    X(int32, Int32)       \
+    X(int64, Int64)       \
+    X(uint8, UInt8)       \
+    X(uint16, UInt16)     \
+    X(uint32, UInt32)     \
+    X(uint64, UInt64)     \
+    X(float32, Float32)   \
+    X(float64, Float64)
 
-//X(int8, Int8)\
-//X(int16, Int16)\
-//X(int32, Int32)\
-//X(int64, Int64)\
-//X(uint8, UInt8)\
-//X(uint16, UInt16)\
-//X(uint32, UInt32)\
-//X(uint64, UInt64)\
-//X(float32, Float32)\
-//X(float64, Float64)\ 
-//
 namespace meta {
-#define DEFINE_METADATA(type, tname)                                                               \
-    template <>                                                                                    \
-    inline const char *type_name<type>() {                                                         \
-        static const char *name = meta::register_type<type>(#tname);                               \
-        return name;                                                                               \
+#define DEFINE_METADATA(type, tname)                                 \
+    template <>                                                      \
+    inline const char *type_name<type>() {                           \
+        static const char *name = meta::register_type<type>(#tname); \
+        return name;                                                 \
     }
 
 TYPES_METADATA(DEFINE_METADATA)
