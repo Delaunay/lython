62659ad6f59e73eb1e509d0e696a6f41dc7e6b25
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-09-30 20:40:26 -0400

    add visitor

--- src/ast/visitor.h
+++ src/ast/visitor.h
@@ -1,8 +1,7 @@
 #ifndef LYTHON_AST_VISITOR_HEADER
 #define LYTHON_AST_VISITOR_HEADER
 
-#include "ast/expressions.h"
-#include "ast/nodes.h"
+#include "ast/sexpression.h"
 
 namespace lython {
 
@@ -19,24 +18,129 @@ NEW_EXCEPTION(NullPointerError)
  * The main drawbacks is it uses macros to generate the vtable at compile time
  * and the error messages can be a bit arcane
  */
-template <typename Implementation, typename Expr, typename ReturnType, typename... Args>
+template <typename Implementation, typename... Args>
 struct BaseVisitor {
-    // Generate alias to Node Reference depending on if Expr is const or not
-    using Expr_t = Expr;
-    using Node_t =
-        typename std::conditional<std::is_const<Expr>::value, const Node *, Node *>::type;
 
-#define TYPEGEN(name) \
-    using name##_t =  \
-        typename std::conditional<std::is_const<Expr>::value, const name *, name *>::type;
-    NODE_KIND_ENUM(KIND)
+    Array<StmtNode *> exec(Array<StmtNode *> &body, int depth, Args... args) {
+        int k = 0;
+        for (auto &stmt: body) {
+            body[k] = exec(stmt, depth, std::forward(args)...);
+            k += 1;
+        }
+        return body;
+    };
+
+    ModNode *exec(ModNode *mod, int depth, Args... args) {
+        // clang-format off
+        switch (mod->kind) {
+
+            #define X(name, _)
+            #define PASS(a, b)
+            #define SECTION(_)
+            #define MOD(name, fun)\
+                case NodeKind::name: {\
+                    name* m = reinterpret_cast<name*>(mod);\
+                    return fun(m, depth + 1, std::forward(args)...);\
+                }
+
+            NODEKIND_ENUM(X, SECTION, PASS, PASS, MOD, PASS)
+
+            #undef X
+            #undef PASS
+            #undef SECTION
+            #undef MOD
+        
+        }
+        // clang-format on
+        return nullptr;
+    }
+
+    Pattern *exec(Pattern *pat, int depth, Args... args) {
+        // clang-format off
+        switch (pat->kind) {
+
+            #define X(name, _)
+            #define PASS(a, b)
+            #define SECTION(_)
+            #define MATCH(name, fun)\
+                case NodeKind::name: {\
+                    name* p = reinterpret_cast<name*>(pat);\
+                    return fun(p, depth + 1, std::forward(args)...);\
+                }
+
+            NODEKIND_ENUM(X, SECTION, PASS, PASS, PASS, MATCH)
+
+            #undef X
+            #undef PASS
+            #undef SECTION
+            #undef MATCH
+        
+        }
+        // clang-format on
+        return nullptr;
+    }
+
+    ExprNode *exec(ExprNode *expr, int depth, Args... args) {
+        // clang-format off
+        switch (expr->kind) {
+
+            #define X(name, _)
+            #define PASS(a, b)
+            #define SECTION(_)
+            #define EXPR(name, fun)\
+                case NodeKind::name: {\
+                    name* node = reinterpret_cast<name*>(expr);\
+                    return fun(node, depth + 1, std::forward(args)...);\
+                }
+
+            NODEKIND_ENUM(X, SECTION, EXPR, PASS, PASS, PASS)
+
+            #undef X
+            #undef PASS
+            #undef SECTION
+            #undef EXPR
+        
+        }
+        // clang-format on
+        return nullptr;
+    }
+
+    StmtNode *exec(StmtNode *stmt, int depth, Args... args) {
+        // clang-format off
+        switch (stmt->kind) {
+
+            #define X(name, _)
+            #define PASS(a, b)
+            #define SECTION(_)
+            #define STMT(name, fun)\
+                case NodeKind::name: {\
+                    name* stmt = reinterpret_cast<name*>(stmt);\
+                    return fun(stmt, depth + 1, std::forward(args)...);\
+                }
+
+            NODEKIND_ENUM(X, SECTION, PASS, STMT, PASS, PASS)
+
+            #undef X
+            #undef PASS
+            #undef SECTION
+            #undef STMT
+        
+        }
+        // clang-format on
+        return nullptr;
+    }
+
+#define FUNCTION_GEN(name, fun)                                                              \
+    name *fun(name *node, int depth, Args... args) {                                         \
+        return static_cast<Implementation *>(this)->fun(node, depth, std::forward(args)...); \
+    }
 
 #define X(name, _)
 #define SECTION(name)
-#define EXPR(name, _)  TYPEGEN(name)
-#define STMT(name, _)  TYPEGEN(name)
-#define MOD(name, _)   TYPEGEN(name)
-#define MATCH(name, _) TYPEGEN(name)
+#define EXPR(name, fun)  FUNCTION_GEN(name, fun)
+#define STMT(name, fun)  FUNCTION_GEN(name, fun)
+#define MOD(name, fun)   FUNCTION_GEN(name, fun)
+#define MATCH(name, fun) FUNCTION_GEN(name, fun)
 
     NODEKIND_ENUM(X, SECTION, EXPR, STMT, MOD, MATCH)
 
@@ -47,67 +151,9 @@ struct BaseVisitor {
 #undef MOD
 #undef MATCH
 
-#undef TYPEGEN
+#undef FUNCTION_GEN
 
-    // force inline to make the stacktrace prettier
-    KIWI_INLINE ReturnType visit(Expr_t expr, std::size_t depth = 0, Args... args) {
-        Node_t node = expr.template ref<Node_t>();
-        return visit(node, depth, args...);
-    }
-
-    // Dispatch table
-    ReturnType visit(Node_t expr, std::size_t depth = 0, Args... args) {
-        switch (expr->kind) {
-
-#define CASEGEN(name, funname)                   \
-case NodeKind::name: {                           \
-    auto ref = reinterpret_cast<name##_t>(expr); \
-    return funname(ref, depth + 1, args...);     \
-}
-
-#define X(name, _)
-#define SECTION(name)
-#define EXPR(name, funname)  CASEGEN(name, funname)
-#define STMT(name, funname)  CASEGEN(name, funname)
-#define MOD(name, funname)   CASEGEN(name, funname)
-#define MATCH(name, funname) CASEGEN(name, funname)
-
-            NODEKIND_ENUM(X, SECTION, EXPR, STMT, MOD, MATCH)
-
-#undef X
-#undef SECTION
-#undef EXPR
-#undef STMT
-#undef MOD
-#undef MATCH
-
-#undef CASEGEN
-
-        default:
-            return undefined(expr, depth + 1, args...);
-        }
-    }
-
-    ReturnType undefined(Node_t node, std::size_t depth, Args... args) {
-        return static_cast<Implementation *>(this)->undefined(node, depth, args...);
-    }
-
-// inline will make the stacktrace better and throw an error if the function is not implemented
-#define KIND(name, funname)                                                             \
-    KIWI_INLINE ReturnType funname(name##_t node, std::size_t depth, Args... args) {    \
-        return reinterpret_cast<Implementation *>(this)->funname(node, depth, args...); \
-    }
-    NODE_KIND_ENUM(KIND)
-#undef KIND
-};
-
-// Const vistor means the Expression are not modified
-// the state of the visitor is still mutable
-template <typename Implementation, typename ReturnType, typename... Args>
-using ConstVisitor = BaseVisitor<Implementation, const Expression, ReturnType, Args...>;
-
-template <typename Implementation, typename ReturnType, typename... Args>
-using Visitor = BaseVisitor<Implementation, Expression, ReturnType, Args...>;
+}; // namespace lython
 
 } // namespace lython
 #endif
--- src/sema/sema.h
+++ src/sema/sema.h
@@ -1,7 +1,39 @@
- 
+#ifndef LYTHON_SEMA_HEADER
+#define LYTHON_SEMA_HEADER
 
-class SemanticAnalyser{
-public:
+#include "ast/visitor.h"
 
+namespace lython {
 
+struct SemanticAnalyser: BaseVisitor<SemanticAnalyser> {
+    public:
+    Module *module(Module *stmt, int depth) {
+        exec(stmt->body, depth);
+        return nullptr;
+    };
+
+#define FUNCTION_GEN(name, fun) \
+    name *fun(name *n, int depth) { return n; }
+
+#define X(name, _)
+#define SECTION(name)
+#define MOD(name, fun)
+#define EXPR(name, fun)  FUNCTION_GEN(name, fun)
+#define STMT(name, fun)  FUNCTION_GEN(name, fun)
+#define MATCH(name, fun) FUNCTION_GEN(name, fun)
+
+    NODEKIND_ENUM(X, SECTION, EXPR, STMT, MOD, MATCH)
+
+#undef X
+#undef SECTION
+#undef EXPR
+#undef STMT
+#undef MOD
+#undef MATCH
+
+#undef FUNCTION_GEN
 };
+
+} // namespace lython
+
+#endif
