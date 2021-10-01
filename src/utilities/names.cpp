438b7050415a0e82ba450eed78f9e0d3f25fed79
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-10-01 05:50:40 -0400

    -

--- src/utilities/names.cpp
+++ src/utilities/names.cpp
@@ -1,7 +1,6 @@
 #include "names.h"
 
 namespace lython {
-StringRef get_string(String const &str) { return StringDatabase::instance().string(str); }
 
 std::ostream &operator<<(std::ostream &out, StringRef ref) {
     return out << StringDatabase::instance()[ref.ref];
@@ -14,25 +13,110 @@ String StringRef::__str__() const {
 
 StringRef::operator StringView() const { return StringDatabase::instance()[ref]; }
 
-StringRef::~StringRef() { StringDatabase::instance().strings[ref].in_use += 1; }
+StringRef::~StringRef() { StringDatabase::instance().dec(ref); }
 
 std::ostream &StringDatabase::report(std::ostream &out) const {
     std::size_t saved    = 0;
     std::size_t saved_up = 0;
 
-    out << fmt::format("| {:30} | {:4} | {:4} | {:4} | {:4} |\n", "str", "#", "use", "low", "upp");
+    out << fmt::format("| {:30} | {:4} | {:4} | {:4} | {:4} | {:4} |\n", "str", "#", "use", "cpy",
+                       "low", "upp");
 
     for (auto &entry: strings) {
         auto lower = entry.data.size() * (entry.count - 1);
-        auto upper = entry.data.size() * (entry.in_use - 1);
+        auto upper = entry.data.size() * (entry.copy - 1);
 
-        out << fmt::format("| {:30} | {:4} | {:4} | {:4} | {:4} |\n", entry.data, entry.count,
-                           entry.in_use, lower, upper);
+        out << fmt::format("| {:30} | {:4} | {:4} | {:4} | {:4} | {:4} |\n", entry.data,
+                           entry.count, entry.in_use, entry.copy, lower, upper);
         saved += lower;
         saved_up += upper;
     }
 
     out << fmt::format("{} < Saved < {} bytes\n", saved, saved_up);
+    out << fmt::format("Spent {} ms waiting on lock\n", wait_time);
     return out;
 }
+
+StringDatabase &StringDatabase::instance() {
+    static StringDatabase db;
+    return db;
+}
+
+StringView StringDatabase::operator[](std::size_t i) const {
+    // we need to lock here in case the array gets reallocated
+    // during a parallel insert
+    StopWatch<>                           timer;
+    std::lock_guard<std::recursive_mutex> guard(mu);
+    wait_time += timer.stop();
+
+    assert(i < strings.size(), "array out of bound");
+
+    if (i < strings.size()) {
+        [[likely]] return strings[i].data;
+    }
+
+    [[unlikely]] return StringView();
+}
+
+std::size_t StringDatabase::dec(std::size_t n) {
+    if (n == 0) {
+        return n;
+    }
+
+    // we need to lock here in case the array gets reallocated during a parallel insert
+    StopWatch<>                           timer;
+    std::lock_guard<std::recursive_mutex> guard(mu);
+    wait_time += timer.stop();
+
+    strings[n].in_use -= 1;
+
+    // Easiest way to keep track of every StringRef in the code
+    strings[n].copy += 1;
+    return n;
+}
+
+std::size_t StringDatabase::inc(std::size_t i) {
+    if (i == 0) {
+        return i;
+    }
+
+    // we need to lock here in case the array gets reallocated during a parallel insert
+    StopWatch<>                           timer;
+    std::lock_guard<std::recursive_mutex> guard(mu);
+    wait_time += timer.stop();
+
+    strings[i].in_use += 1;
+    return i;
+};
+
+StringRef StringDatabase::string(String const &name) {
+    StopWatch<>                           timer;
+    std::lock_guard<std::recursive_mutex> guard(mu);
+    wait_time += timer.stop();
+
+    auto val = defined.find(name);
+
+    if (val == defined.end()) {
+        std::size_t n = strings.size();
+
+        strings.push_back({name, 1, 0, 1});
+        StringView str = strings[n].data;
+
+        defined[str] = n;
+        return StringRef(n);
+    }
+
+    auto ref = val->second;
+    strings[ref].count += 1;
+    strings[ref].in_use += 1;
+    return StringRef(ref);
+}
+
+StringDatabase::StringDatabase() {
+    strings.reserve(128);
+    strings.push_back({"", 0, 0});
+    StringView str = strings[0].data;
+    defined[str]   = 0;
+}
+
 } // namespace lython
