2d141ac9c205dad35a235180ec93ee5cb89d578b
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-12-21 02:07:17 -0500

    -

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -408,12 +408,9 @@ function(setup_target_for_coverage_gcovr_html)
             --object-directory=${PROJECT_BINARY_DIR}
             --xml-pretty
             -x ${Coverage_NAME}/coverage.xml
-            --csv ${Coverage_NAME}/coverage.csv
-            --json-pretty
-            --json ${Coverage_NAME}/coverage.json
             -o ${Coverage_NAME}/index.html
-            -j 4
             --exclude-directories /usr/
+            --exclude-directories "${PROJECT_SOURCE_DIR}/dependencies"
             --exclude-directories "${PROJECT_SOURCE_DIR}/tests"
 
         BYPRODUCTS ${PROJECT_BINARY_DIR}/${Coverage_NAME}  # report directory
