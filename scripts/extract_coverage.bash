ec56a7c7d3dde40e07e7090324b562cd1724adf0
Setepenre <pierre@delaunay.io>
2021-12-29 19:32:25 -0500

    -

--- scripts/extract_coverage.bash
+++ scripts/extract_coverage.bash
@@ -1,9 +1,8 @@
-set -v
 
 SEARCH='<coverage line-rate="\([0-9.]*\)" branch-rate="\([0-9.]*\)" lines-covered="\([0-9.]*\)" lines-valid="\([0-9.]*\)" branches-covered="\([0-9.]*\)" branches-valid="\([0-9.]*\)" complexity="\([0-9.]*\)" timestamp="\([0-9.]*\)" version="\([a-zA-Z 0-9.]*\)">'
 
 HEADER="timestamp, line-rate, branch-rate, lines-covered, lines-valid, branches-covered, branches-valid, complexity, version"
-REPLACE='\8, \1, \2, \3, \4, \5, \6, \7, \9'
+REPLACE='\8,\1,\2,\3,\4,\5,\6,\7,\9'
 
 RESULT=$(cat $1 | sed -n "s/$SEARCH/$REPLACE/p")
 
