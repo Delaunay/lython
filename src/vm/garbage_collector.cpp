a6771b550faacdfe76124020f62315af25ba27af
Setepenre <pierre.delaunay@outlook.com>
2022-11-15 23:15:29 -0500

    Reflect on a GC

---
+++ src/vm/garbage_collector.cpp
@@ -0,0 +1,59 @@
+#include "garbage_collector.h"
+#include "sema/bindings.h"
+
+namespace lython {
+
+GargabeCollector::GargabeCollector(struct Bindings& bindings, struct Expression& root):
+    bindings(bindings), root(root)  //
+{}
+
+void GargabeCollector::mark() {
+    // For generational collection we could start
+    // the check later into the binding array
+    int i = bindings.global_index;
+
+    for (; i < bindings.bindings.size(); i++) {
+        //
+        BindingEntry& entry = bindings.bindings[i];
+
+        // Mark the object as viewed
+        GCObject* value = entry.value;
+
+        // TODO: fetch the GC metadata
+        RTGCObject* mapped = nullptr;
+        if (mapped) {
+            mapped->cycle = cycle;
+        }
+    }
+}
+
+void GargabeCollector::free_object(RTGCObject* obj) {
+    root.remove_child((GCObject*)obj->object, true);
+}
+
+void GargabeCollector::sweep() { shrink(); }
+
+void GargabeCollector::shrink() {
+    int        deleted   = 0;
+    int        i         = 0;
+    int        free_spot = 0;
+    Array<int> free_spots;
+
+    for (RTGCObject& obj: tracked) {
+        if (obj.cycle < cycle) {
+            deleted += 1;
+            free_object(&obj);
+            free_spots.push_back(i);
+        } else if (!free_spots.empty()) {
+            tracked[free_spot] = obj;
+            tracked[i]         = RTGCObject{0, nullptr};
+            free_spots.push_back(i);
+            free_spot += 1;
+        }
+        i += 1;
+    }
+
+    tracked.resize(int(tracked.size()) - deleted);
+    cycle += 1;
+}
+}  // namespace lython
---
+++ src/vm/garbage_collector.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "dtypes.h"
+#include "utilities/object.h"
+
+namespace lython {
+
+// runtime gcobject, as opposed to the compiler gcobject that behave differently
+struct RTGCObject {
+    int   cycle  = false;  // collection cycle, collect if gc cycle <= collection cycle
+    void* object = nullptr;
+};
+
+// Very simple garbage collector
+class GargabeCollector {
+    GargabeCollector(struct Bindings& bindings, struct Expression& root);
+
+    // Allocation hook
+    template <typename T, typename... Args>
+    T* new_object(Args... args) {
+        T* obj = root.new_object(args...);
+        tracked.push_back(RTGCObject{false, obj});
+        return obj
+    }
+
+    void free_object(RTGCObject* obj);
+
+    void mark();
+
+    void sweep();
+
+    // Compact and collect memory
+    void shrink();
+
+    private:
+    // this is the state of the execution
+    struct Bindings& bindings;
+
+    // this is the root node that holds all the temporaries
+    struct GCObject& root;
+
+    int               cycle = 0;
+    Array<RTGCObject> tracked;
+};
+}  // namespace lython
